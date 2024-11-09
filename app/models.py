c4cd76dff88345c68e94ae35178d425872edd917
Mark Harmsen <mharmsenr@gmail.com>
2024-11-09 17:14:21 -0500

    routes and models

---
+++ app/models.py
@@ -0,0 +1,7 @@
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import db
+
+class Email(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    body: so.Mapped[str] = so.mapped_column(sa.Text)
