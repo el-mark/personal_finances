49368d356cc63a1c0c7f40f07335bec318ae84d7
Mark Harmsen <mharmsenr@gmail.com>
2024-11-11 10:44:29 -0500

    email_form

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 if __name__ == '__main__':
     app.run(debug=True)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db' 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.secret_key = 'your_unique_secret_key'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,21 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect, url_for, flash
 from app.models import Email
+from app import db 
+
 
 @app.route('/')
 def index():
     emails = Email.query.all()
     return render_template('index.html', emails=emails)
+
+@app.route('/email_form', methods=['GET', 'POST'])
+def email_form():
+    if request.method == 'POST':
+        body = request.form['body']
+        new_email = Email(body=body)
+        db.session.add(new_email)
+        db.session.commit()
+        flash('Email submitted successfully!')
+        return redirect(url_for('index'))
+    return render_template('email_form.html')
